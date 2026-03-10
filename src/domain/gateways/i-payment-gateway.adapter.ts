d66a8365ae4e6d3abe153848927e36ecaa20a41b
@Odisseu93 - Ulisses Silvério <ulissessuporteti01@gmail.com>
2026-03-10 19:12:09 -0300

    feat(domain): add IPaymentGatewayAdapter interface

---
+++ src/domain/gateways/i-payment-gateway.adapter.ts
@@ -0,0 +1,21 @@
+import { Money } from '#domain/value-objects/money.vo'
+
+export interface ChargeInput {
+  amount: Money
+  name: string
+  email: string
+  cardNumber: string
+  cvv: string
+  expiryMonth?: number
+  expiryYear?: number
+}
+
+export interface ChargeOutput {
+  externalId: string
+  status: 'paid' | 'failed'
+}
+
+export interface IPaymentGatewayAdapter {
+  charge(input: ChargeInput): Promise<ChargeOutput>
+  refund(externalId: string): Promise<boolean>
+}
